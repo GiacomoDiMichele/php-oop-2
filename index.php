d375ecf63c0f6e7854a5677c7c9f7cd818fc9902
Giacomo <giacomo.1936@gmail.com>
2021-01-18 18:09:50 +0100

    aggiunta secondo user

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ require_once 'user.php';
 
 $first_user = new User('Marco', 'Grandi');
 
-    $first_user->findChar('giacomo04mail.com');
+    $first_user->findChar('marco@mail.com');
     var_dump($first_user);
 
-
-
-
 $second_user = new User('Mario', 'Rossi');
+
+    $second_user->findChar('mario90.mail.com');
+    var_dump($second_user);
 
 
 
